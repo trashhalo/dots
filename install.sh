ea4277f891ff2e6b28e6bf287dbf992669c93bbc
Stephen Solka <stephen@solka.dev>
2024-06-10 16:24:47 -0400

    tweak just bootstrap

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-if ! command -v just &> /dev/null
-then
-    curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to $(pwd)
-else
-    echo "just command found, skipping installation"
-fi
-
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to $(pwd)
 ./just install
