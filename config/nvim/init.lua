4fd2ca2da4291992c005fdeec96214b7698b377d
Stephen Solka <stephen@solka.dev>
2024-08-04 22:05:38 -0400

    on_highlights

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -404,6 +404,193 @@ require("lazy").setup({
 					colors.bg_added = "#e6ffec"
 					colors.bg_changed = "#fff5cc"
 					colors.bg_removed = "#ffebe9"
+				end,
+				on_highlights = function(highlight, color)
+					-- Syntax highlighting
+					highlight.Comment = { fg = color.brown, italic = true }
+					highlight.String = { fg = color.blue }
+					highlight.Number = { fg = color.orange }
+					highlight.Boolean = { fg = color.green, bold = true }
+					highlight.Function = { fg = color.red, bold = true }
+					highlight.Keyword = { fg = color.magenta }
+					highlight.Conditional = { fg = color.purple }
+					highlight.Operator = { fg = color.cyan }
+					highlight.Type = { fg = color.yellow }
+
+					-- UI elements
+					highlight.CursorLine = { bg = color.bg_dim }
+					highlight.Visual = { bg = color.yellow, fg = color.bg_main }
+					highlight.Search = { bg = color.green, fg = color.bg_main }
+					highlight.IncSearch = { bg = color.orange, fg = color.bg_main }
+					highlight.LineNr = { fg = color.blue }
+					highlight.CursorLineNr = { fg = color.red, bold = true }
+
+					-- Status line
+					highlight.StatusLine = { bg = color.blue, fg = color.bg_main }
+					highlight.StatusLineNC = { bg = color.bg_dim, fg = color.fg_dim }
+
+					-- Tabs
+					highlight.TabLine = { bg = color.bg_dim, fg = color.fg_dim }
+					highlight.TabLineSel = { bg = color.red, fg = color.bg_main, bold = true }
+					highlight.TabLineFill = { bg = color.bg_main }
+
+					-- Popup menu
+					highlight.Pmenu = { bg = color.bg_dim, fg = color.fg_main }
+					highlight.PmenuSel = { bg = color.blue, fg = color.bg_main }
+
+					-- Git diff
+					highlight.DiffAdd = { bg = color.green, fg = color.bg_main }
+					highlight.DiffChange = { bg = color.yellow, fg = color.bg_main }
+					highlight.DiffDelete = { bg = color.red, fg = color.bg_main }
+
+					-- Errors and warnings
+					highlight.Error = { fg = color.red, bold = true }
+					highlight.WarningMsg = { fg = color.orange, bold = true }
+
+					-- Miscellaneous Syntax related non-standard highlights
+					highlight.qfLineNr = { fg = color.fg_dim }
+					highlight.qfFileName = { fg = color.blue }
+
+					-- HTML headers
+					highlight.htmlH1 = { fg = color.red, bold = true }
+					highlight.htmlH2 = { fg = color.blue, bold = true }
+					highlight.htmlH3 = { fg = color.green, bold = true }
+					highlight.htmlH4 = { fg = color.yellow, bold = true }
+					highlight.htmlH5 = { fg = color.purple, bold = true }
+					highlight.htmlH6 = { fg = color.cyan, bold = true }
+
+					-- Markdown
+					highlight.mkdCodeDelimiter = { bg = color.bg_dim, fg = color.fg_main }
+					highlight.mkdCodeStart = { fg = color.cyan, bold = true }
+					highlight.mkdCodeEnd = { fg = color.cyan, bold = true }
+
+					highlight.markdownHeadingDelimiter = { fg = color.orange, bold = true }
+					highlight.markdownCode = { fg = color.cyan }
+					highlight.markdownCodeBlock = { fg = color.cyan }
+					highlight.markdownLinkText = { fg = color.blue, underline = true }
+					highlight.markdownH1 = { fg = color.red, bold = true }
+					highlight.markdownH2 = { fg = color.blue, bold = true }
+					highlight.markdownH3 = { fg = color.green, bold = true }
+					highlight.markdownH4 = { fg = color.yellow, bold = true }
+					highlight.markdownH5 = { fg = color.purple, bold = true }
+					highlight.markdownH6 = { fg = color.cyan, bold = true }
+
+					-- LSP highlights
+					highlight.LspCodeLens = { fg = color.fg_dim }
+					highlight.LspInlayHint = { bg = color.bg_main, fg = color.fg_dim, italic = true }
+					highlight.LspReferenceText = { bg = color.bg_dim, fg = color.fg_main }
+					highlight.LspReferenceRead = { bg = color.bg_dim, fg = color.fg_main }
+					highlight.LspReferenceWrite = { bg = color.bg_dim, fg = color.fg_main }
+					highlight.LspInfoBorder = { fg = color.blue, bg = color.bg_main }
+
+					-- Diagnostics
+					highlight.DiagnosticError = { fg = color.red, bold = true }
+					highlight.DiagnosticWarn = { fg = color.yellow, bold = true }
+					highlight.DiagnosticInfo = { fg = color.blue, bold = true }
+					highlight.DiagnosticHint = { fg = color.green, bold = true }
+					highlight.DiagnosticUnnecessary = { fg = color.fg_dim }
+
+					highlight.DiagnosticVirtualTextError = { fg = color.red, bold = true }
+					highlight.DiagnosticVirtualTextWarn = { fg = color.yellow, bold = true }
+					highlight.DiagnosticVirtualTextInfo = { fg = color.blue, bold = true }
+					highlight.DiagnosticVirtualTextHint = { fg = color.green, bold = true }
+
+					highlight.DiagnosticUnderlineError = { undercurl = true, sp = color.red }
+					highlight.DiagnosticUnderlineWarn = { undercurl = true, sp = color.yellow }
+					highlight.DiagnosticUnderlineInfo = { undercurl = true, sp = color.blue }
+					highlight.DiagnosticUnderlineHint = { undercurl = true, sp = color.green }
+
+					highlight.ALEErrorSign = { fg = color.red, bold = true }
+					highlight.ALEWarningSign = { fg = color.yellow, bold = true }
+					-- Neovim tree-sitter highlights
+					-- Identifiers
+					highlight["@variable.parameter"] = { fg = color.cyan }
+					highlight["@variable.parameter.builtin"] = { fg = color.cyan, italic = true }
+
+					-- Literals
+					highlight["@string.regex"] = { fg = color.green }
+					highlight["@string.escape"] = { fg = color.yellow }
+					highlight["@string.special"] = { fg = color.red }
+					highlight["@string.special.path"] = { fg = color.blue }
+					highlight["@string.special.url"] = { fg = color.cyan }
+
+
+
+					-- Types
+
+
+					-- Functions
+
+					highlight["@constructor"] = { fg = color.yellow }
+					-- Keywords
+					-- Punctuation
+					highlight["@punctuation.bracket"] = { fg = color.fg_main }
+					highlight["@punctuation.special"] = { fg = color.fg_main }
+
+					-- Comments
+					highlight["@comment.error"] = { fg = color.red }
+					highlight["@comment.warning"] = { fg = color.yellow }
+					highlight["@comment.note"] = { fg = color.green }
+
+					-- Markup
+					highlight["@markup.strong"] = { bold = true }
+					highlight["@markup.italic"] = { italic = true }
+					highlight["@markup.strikethrough"] = { strikethrough = true }
+					highlight["@markup.underline"] = { underline = true }
+					highlight["@markup.heading.1"] = { fg = color.red, bold = true }
+					highlight["@markup.heading.2"] = { fg = color.blue, bold = true }
+					highlight["@markup.heading.3"] = { fg = color.green, bold = true }
+					highlight["@markup.heading.4"] = { fg = color.yellow, bold = true }
+					highlight["@markup.heading.5"] = { fg = color.purple, bold = true }
+					highlight["@markup.heading.6"] = { fg = color.cyan, bold = true }
+					highlight["@markup.quote"] = { italic = true }
+					highlight["@markup.link"] = { fg = color.cyan }
+					highlight["@markup.list"] = { fg = color.fg_main }
+					highlight["@markup.list.checked"] = { fg = color.green }
+					highlight["@markup.list.unchecked"] = { fg = color.yellow }
+
+					-- Diff
+
+					-- Tags
+
+					highlight["@none"] = {}
+
+					-- TSX specific
+					highlight["@tag.tsx"] = { fg = color.red }
+					highlight["@constructor.tsx"] = { fg = color.blue }
+					highlight["@tag.delimiter.tsx"] = { fg = color.blue }
+
+					-- Python specific
+
+					-- Telescope
+					highlight.TelescopeBorder = { fg = color.blue, bg = color.bg_main }
+					highlight.TelescopeTitle = { fg = color.fg_dim, bg = color.bg_main }
+					highlight.TelescopePromptBorder = { fg = color.red, bg = color.bg_main }
+					highlight.TelescopePromptTitle = { fg = color.red, bg = color.bg_main }
+					highlight.TelescopeResultsComment = { fg = color.fg_dim }
+
+					-- NvimTree
+					highlight.NvimTreeNormal = { fg = color.fg_main, bg = color.bg_dim }
+					highlight.NvimTreeWinSeparator = { fg = color.blue, bg = color.blue }
+					highlight.NvimTreeNormalNC = { fg = color.fg_dim, bg = color.bg_dim }
+					highlight.NvimTreeRootFolder = { fg = color.red, bold = true }
+					highlight.NvimTreeGitDirty = { fg = color.yellow }
+					highlight.NvimTreeGitNew = { fg = color.green }
+					highlight.NvimTreeGitDeleted = { fg = color.red }
+					highlight.NvimTreeOpenedFile = { bg = color.bg_dim }
+					highlight.NvimTreeSpecialFile = { fg = color.purple, underline = true }
+					highlight.NvimTreeIndentMarker = { fg = color.fg_dim }
+					highlight.NvimTreeImageFile = { fg = color.fg_main }
+					highlight.NvimTreeFolderIcon = { bg = color.none, fg = color.blue }
+
+					-- WhichKey
+					highlight.WhichKey = { fg = color.cyan }
+					highlight.WhichKeyGroup = { fg = color.blue }
+					highlight.WhichKeyDesc = { fg = color.purple }
+					highlight.WhichKeySeperator = { fg = color.fg_dim }
+					highlight.WhichKeySeparator = { fg = color.fg_dim }
+					highlight.WhichKeyFloat = { bg = color.bg_dim }
+					highlight.WhichKeyValue = { fg = color.fg_dim }
 				end
 			})
 			vim.cmd('colorscheme modus_operandi')
